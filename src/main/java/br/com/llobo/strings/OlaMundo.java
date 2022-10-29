754775c8dedfd354f60e1ef71844c67b22fe2d58
laurallobo <laura.llobo@gmail.com>
2022-10-29 10:40:20 -0300

    novos updates

---
+++ src/main/java/br/com/llobo/strings/OlaMundo.java
@@ -0,0 +1,11 @@
+package br.com.llobo.strings;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola mundo");
+
+	}
+
+}
