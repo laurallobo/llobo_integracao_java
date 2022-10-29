9c3a309c855a9907bef23af706f8882c31012161
laurallobo <laura.llobo@gmail.com>
2022-10-29 10:19:54 -0300

    teste02

---
+++ src/main/java/br/com/llobo/strings/Titi.java
@@ -0,0 +1,14 @@
+package br.com.llobo.strings;
+
+public class Titi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String texto = "Ola mundo teste";
+		for (int i = 0; i < texto.length(); i++) {
+			System.out.println(texto.charAt(i));
+		}
+
+	}
+
+}
