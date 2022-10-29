984ea555c695b1e7a038e4254a20c04e328278e5
laurallobo <laura.llobo@gmail.com>
2022-10-29 09:51:24 -0300

    nova funcionalidade

---
+++ src/main/java/br/com/llobo/strings/NovaClasse.java
@@ -0,0 +1,11 @@
+package br.com.llobo.strings;
+
+public class NovaClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sou a nova classe");
+
+	}
+
+}
